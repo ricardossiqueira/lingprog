81a12bfb1fa1b3bd402e2748b8e8c0beed59431f
ricardossiqueira <ricardosantossiqueira@poli.ufrj.br>
2021-09-12 22:25:04 -0300

    📦 entrega

---
+++ trabalho_1/ricardo_santos_siqueira/Menu.cpp
@@ -0,0 +1,185 @@
+/*Trabalho - Linguagens de Programacao
+ *Aluno: Ricardo Santos Siqueira
+ *DRE: 118167558
+ *Periodo: 2021.1
+ *Arquivo: Menu.cpp
+ *Descricao: Implementacao das funcoes do menu
+ */
+
+#include "Menu.hpp"
+
+Menu::Menu()
+{
+  _handleURI();
+}
+
+void Menu::run()
+{
+  int option = 0;
+  string avalilableOptions = "Opcoes:\n\t1 - Cortar faces\n\t2 - Ajustar proporcao da imagem para 3:4\n\t3 - [Filtro] Suavizar detalhes\n\t4 - [Filtro] Realcar detalhes\n\t5 - [Filtro] Ajustar cor\n\t6 - [Filtro] Ajustar contraste\n\t7 - Salvar copias e sair";
+
+  // Enquanto a opcao selecionada nao for 7 (Save), o programa continua recebendo
+  // entradas do usuario. Quando selecionada a opcao o menu retorna e o comando
+  // e enviado para o script python que sempre executa a opcao de salvar
+  while (option != Save)
+  {
+    cout << avalilableOptions << endl;
+    cout << ">>> ";
+
+    option = _handleNumber();
+
+    while (_validateInput(option, Faces, Save) == 0)
+    {
+      cout << ">>> ";
+      option = _handleNumber();
+    }
+
+    switch (option)
+    {
+    case Faces:
+      _faces = _handleBool( //_handleFaces
+          "Opcoes:\n\t1 - Cortar apenas rostos\n\t2 - Voltar");
+      break;
+
+    case AR3x4:
+      _3x4 = _handleBool( // _handle3x4
+          "Opcoes:\n\t1 - Ajustar para proporcao 3:4\n\t2 - Voltar");
+      break;
+
+    case Smooth:
+      _smooth = _handleBool( // _handleSmooth
+          "Opcoes:\n\t1 - Suavizar detalhes da imagem\n\t2 - Voltar");
+      break;
+
+    case Detail:
+      _detail = _handleBool( //_handleDetail
+          "Opcoes:\n\t1 - Realcar detalhes da imagem\n\t2 - Voltar");
+      break;
+
+    case Color:
+      _color = _handleSelection( // _handleColor
+          "Opcoes:\n\t1 - Ajustar intensidade de cor\n\t2 - Voltar");
+      break;
+
+    case Contrast:
+      _contrast = _handleSelection( // _handleContrast
+          "Opcoes:\n\t1 - Ajustar contraste\n\t2 - Voltar");
+      break;
+
+    default:
+      break;
+    }
+  }
+}
+
+string _stringfyBool(bool arg) { return arg ? "true" : "false"; }
+
+string Menu::parseCommand()
+{
+  return "'" + _uri + "' " + _stringfyBool(_faces) + " " + _stringfyBool(_3x4) + " " + _stringfyBool(_smooth) + " " + _stringfyBool(_detail) + " " + to_string(_color) + " " + to_string(_contrast);
+}
+
+bool Menu::_handleBool(string msg)
+{
+  unsigned selected = 0;
+  do
+  {
+    cout
+        << msg
+        << endl;
+    cout << ">>> ";
+    selected = _handleNumber();
+  } while (_validateInput(selected, 1, 2) == 0);
+  if (selected == 1)
+    return true;
+  else
+    return false;
+}
+
+float Menu::_handleSelection(string msg)
+{
+  unsigned selected = 0;
+  float value = -1.f;
+  do
+  {
+    cout
+        << msg
+        << endl;
+    cout << ">>> ";
+    selected = _handleNumber();
+  } while (_validateInput(selected, 1, 2) == 0);
+  if (selected == 1)
+  {
+    do
+    {
+      cout
+          << "Selecione a intensidade desejada entre 0.0 (0%) e 2.0 (200%)"
+          << endl;
+      cout << ">>> ";
+      cin >> value;
+    } while (_validateInput(value, 0.f, 2.f) == -1.f);
+    return value;
+  }
+  else
+    return 1.f;
+}
+
+int Menu::_validateInput(int num, int min, int max)
+{
+  if (num < min || num > max)
+  {
+    cerr
+        << "Erro: Entre com um valor valido ["
+        << min
+        << ", "
+        << max
+        << "]"
+        << endl;
+    return 0;
+  }
+  return num;
+}
+
+float Menu::_validateInput(float num, float min, float max)
+{
+  if (num < min || num > max)
+  {
+    cerr
+        << "Erro: Entre com um valor valido ["
+        << min
+        << ", "
+        << max
+        << "]"
+        << endl;
+    return -1.f;
+  }
+  return num;
+}
+
+void Menu::_handleURI()
+{
+  string uri;
+  cout << "Entre com a URL da imagem:" << endl;
+  cout << ">>> ";
+  cin >> uri;
+  while (!regex_match(uri, regex("((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)")))
+  {
+    cerr << "Erro: Entre com uma URL valida" << endl;
+    cout << ">>> ";
+    cin >> uri;
+  }
+  _uri = uri;
+}
+
+int Menu::_handleNumber()
+{
+  int arg;
+  while (!(cin >> arg))
+  {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cerr << "Erro: Entrada invalida" << endl;
+    cout << ">>> ";
+  }
+  return arg;
+}
---
+++ trabalho_1/ricardo_santos_siqueira/README.md
@@ -0,0 +1,72 @@
+# Trabalho - Linguagens de Programação
+
+**Aluno:** Ricardo Santos Siqueira\
+**DRE:** 118167558\
+**Período:** 2021.1 
+
+## Ambiente de desenvolvimento
+
+**OS:** Manjaro Linux x86_64\
+**Kernel:** 5.13.12-1-MANJARO\
+**Shell:** fish **3.3**.1
+
+## Objetivo
+
+  Programa para download e tratamento básico de imagens feito em C++ e [Python](./python/README.md).  
+
+## Dependências
+
+Esse projeto depende de alguma bibliotecas python para funcionar, caso já não tenha instalado, favor instalar antes de tentar executar. Mais informações no [README do script Python](./python/README.md).
+
+**Antes de compilar e executar tenha ceteza de visitar a referência acima.**
+
+## Compilação
+
+```console
+make all
+```
+
+## Limpeza
+
+```console
+make clean
+```
+
+## Utilização
+
+```console
+# Antes de executar tenha certeza que o ambiente pipenv está ativo
+
+./main
+```
+  Após a inicialização do programa o usuário inicialmente deve entrar com a URL referente a imagem que deseja utilizar. Uma vez inicializada corretamente a url o programa apresentará um menu com as opções disponíveis, sendo estas:
+
+  1 - Cortar faces\
+  2 - Ajustar proporcao da imagem para 3:4\
+  3 - [Filtro] Suavizar detalhes\
+  4 - [Filtro] Realcar detalhes\
+  5 - [Filtro] Ajustar cor\
+  6 - [Filtro] Ajustar contraste\
+  7 - Salvar copias e sair
+
+  Ao selecionar uma opção um submenu é exibido e o usuário entra com os parâmetros para executar a ação ou pode retornar ao menu principal.
+
+  O programa é finalizado ao selecionar a opção 7, correspondente a salvar as imagens geradas e sair do programa.
+
+## Exemplo de imagem editada
+
+   [Original](https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80) | Editada
+  :-:|:-:
+  ![Original](assets/original.png "Original") | ![Editada](assets/edited.png "Editada")
+  
+  Parâmetros da edição: Cortar face, ajuste 3:4, suavizar detalhes, ajuste de cor 1.3 (130%) e ajuste de contraste 1.1 (110%).
+
+## Referências externas
+
+[How to make std::cin take only numbers (stackoverflow)](https://stackoverflow.com/questions/10828937/how-to-make-cin-take-only-numbers)
+
+[Call destructor in C++](https://www.delftstack.com/howto/cpp/call-destructor-in-cpp/)
+
+## Créditos
+
+Esse programa utiliza o algoritimo [Haarcascade Frontal Face Alt2](https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_alt2.xml) disponível na biblioteca OpenCV para detecção das faces nas imagens.
